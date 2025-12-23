3b5ac3424fd521b93525c0273a7e7f723c3451d2
janne6565 <jabbekeipert@gmail.com>
2025-12-23 22:24:21 +0100

    Update API context path, adjust controller mapping, and add Actuator dependency

--- src/main/java/com/janne/dreialbenapiproxy/controller/AlbumController.java
+++ src/main/java/com/janne/dreialbenapiproxy/controller/AlbumController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 @RestController
-@RequestMapping("/api/v1/albums")
+@RequestMapping("/v1/albums")
 @RequiredArgsConstructor
 public class AlbumController {
 
