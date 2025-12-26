7005a818a407e95e8c0a2310b3885443b3086a94
janne6565 <jabbekeipert@gmail.com>
2025-12-26 05:36:29 +0100

    Add CORS configuration to allow requests from localhost:8081

---
+++ src/main/java/com/janne/dreialbenapiproxy/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.janne.dreialbenapiproxy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class SecurityConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins(List.of(
+                "http://localhost:8081").toArray(String[]::new))
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+    }
+}
